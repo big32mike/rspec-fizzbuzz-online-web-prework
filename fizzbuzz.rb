a8510e5c854c675f1cffe61d328ca6282e9b837d
michael white <michaeljwhite@gmail.com>
2019-01-29 08:39:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(value)
-  result = ''
-  if (value % 3) == 0 
-    result += 'Fizz'
+  if ((value % 3) == 0 && (value % 5) == 0)
+    result = 'FizzBuzz'
+  elsif (value % 3) == 0
+    result = 'Fizz'
+  elsif (value % 5) == 0
+    result = 'Buzz'
+  else
+    result = nil
   end
-  if (value % 5) == 0 
-    result += 'Buzz'
-  end
-  unless result != '' return nil
+  result
 end
